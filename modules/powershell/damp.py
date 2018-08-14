b2138146fcf4d6c2f0b8ef5403b54a2b9f943649
GregLucand <28956517+GregLucand@users.noreply.github.com>
2018-08-14 09:19:42 +0200

    Add files via upload

---
+++ modules/powershell/damp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Discretionary ACL Modification Project
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Grégory Lucand"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Discretionary ACL Modification Project"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/HarmJ0y/DAMP"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="DAMP"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
