27e106db8d36c2b4cc9a55cf423fee6e3fb8adb1
GregLucand <28956517+GregLucand@users.noreply.github.com>
2018-08-15 09:53:42 +0200

    Add files via upload

---
+++ modules/powershell/invoke-credentialphisher.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Invoke-CredentialPhisher
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Grégory Lucand"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Invoke-CredentialPhisher"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/fox-it/Invoke-CredentialPhisher"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="Invoke-CredentialPhisher"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
